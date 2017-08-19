b8ee26e6bec1cf356eba16c90c167700078df1a3
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2017-08-19 16:37:15 +0530

    function returning value

--- factorial.go
+++ factorial.go
@@ -4,15 +4,17 @@ import "fmt"
 
 func main() {
 	var n  = 5
-	factorial(n)
+	result := factorial(n)
+	fmt.Println("result is ", result)
 }
 
-func factorial(n int)  {
+func factorial(n int) int {
 	var factorial int = 1;
 	for i := 2; i <= n; i++ {
 		//fmt.Println(i)
 		factorial *= i
 	}
 
-	fmt.Println("Factorial is ", factorial)
+	//fmt.Println("Factorial is ", factorial)
+	return factorial
 }
