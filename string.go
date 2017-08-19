8a4959b0526d7c2157cc16edcd4eb552e3ca20b9
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2017-08-19 16:42:50 +0530

    string as array example

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name  = "a hello world"
+	//prints ascii of the char
+	fmt.Println(name[0])
+
+	//different type of declarations
+	x := 10
+	fmt.Println(x)
+
+}
