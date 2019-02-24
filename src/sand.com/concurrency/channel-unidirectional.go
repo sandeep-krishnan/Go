693a92c859a1dc971163b0eec6b7cff58f8ef93c
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2019-02-24 10:35:06 +0530

    unidirection channels

---
+++ src/sand.com/concurrency/channel-unidirectional.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 4)
+
+	fmt.Println("Length of channel before adding ", len(ch))
+	fmt.Println("Capacity of channel before adding ", cap(ch))
+	addNumbersToChannel(ch)
+
+	fmt.Println("Length of channel after adding ", len(ch))
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
+
+func addNumbersToChannel(c chan<- int) {
+	c <- 1
+	c <- 2
+
+	//compile error - receive from send only channel
+	//fmt.Println(<- c)
+}
