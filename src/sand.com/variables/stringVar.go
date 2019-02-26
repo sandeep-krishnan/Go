4bca84da2eb85e5eab3b489b4c004c932316fb57
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2019-02-26 08:35:12 +0530

    string & struct pointers

--- src/sand.com/variables/stringVar.go
+++ src/sand.com/variables/stringVar.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+type customer struct {
+	id   int
+	name string
+}
+
 func main() {
-	var name  = "a hello world"
+	var name = "a hello world"
 	//prints ascii of the char
 	fmt.Println(name[0])
 
@@ -11,4 +16,51 @@ func main() {
 	x := 10
 	fmt.Println(x)
 
+	//the value will not be modified
+	//by default the funcs use pass by value
+	printStr(name)
+	fmt.Println("String value after modification ", name)
+
+	//the value is modified in this call
+	printStrPointer(&name)
+
+	fmt.Println("String value after modification using pointer ", name)
+
+	createCustomer()
+}
+
+func printStrPointer(strPtr *string) {
+	fmt.Println("Printing string pointer value --> ", *strPtr)
+	fmt.Println("Printing string pointer address --> ", strPtr)
+
+	fmt.Println("modifying string pointer value ")
+	*strPtr = " Hello World"
+}
+
+func printStr(str string) {
+	fmt.Println("Printing string value --> ", str)
+
+	fmt.Println("modifying string value ")
+	str = " Hello World"
+}
+
+func createCustomer() {
+	//new returns *T, a pointer to the type
+	cust := new(customer)
+	cust.name = "God"
+	cust.id = 1
+
+	printCustomer(cust)
+	modifyCustomer(cust)
+	printCustomer(cust)
+
+}
+
+func printCustomer(cust *customer) {
+	fmt.Println("Customer ", cust.id, " ", cust.name)
+}
+
+func modifyCustomer(cust *customer) {
+	fmt.Println("Modifying customer id to 2")
+	cust.id = 2
 }
