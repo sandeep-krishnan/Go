863ceeb2cc9cf586c541c2afe480fc258fc38a3a
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2019-02-26 21:07:35 +0530

    different ways to initialize arrays

--- src/sand.com/variables/arrays.go
+++ src/sand.com/variables/arrays.go
@@ -7,17 +7,32 @@ func main() {
 	//will not modify x as a copy is made
 	initialize(x)
 	fmt.Println(x)
+
+	initializeUsingPtr(&x)
+	fmt.Println(x)
+
+	initializeUsingSlice(x[:])
+	fmt.Println(x)
 }
 
 // the array is copied as part of this method
 func initialize(numbers [5]int) {
-	for i, _ := range numbers {
+	fmt.Println("initializing to 5")
+	for i := range numbers {
+		numbers[i] = 5
+	}
+}
+
+func initializeUsingPtr(numbers *[5]int) {
+	fmt.Println("initializing using pointers to 10")
+	for i := 0; i < len(numbers); i++ {
 		numbers[i] = 10
 	}
 }
 
-func initializeUsingPtr(numbers *[]int) {
-	for i, _ := range numbers {
-		numbers[i] = 10
+func initializeUsingSlice(numbers []int) {
+	fmt.Println("initializing using slices to 20")
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = 20
 	}
 }
